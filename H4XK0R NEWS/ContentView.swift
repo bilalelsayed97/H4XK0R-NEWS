afe7fa07edd957a363873d5df16a8b8e60876bb0
bilal elsayed <bilalelsayed97@gmail.com>
2024-02-22 21:08:59 +0200

    Initial Commit

---
+++ H4XK0R NEWS/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  H4XK0R NEWS
+//
+//  Created by bilal on 22/02/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ H4XK0R NEWS/H4XK0R_NEWSApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4XK0R_NEWSApp.swift
+//  H4XK0R NEWS
+//
+//  Created by bilal on 22/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct H4XK0R_NEWSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
